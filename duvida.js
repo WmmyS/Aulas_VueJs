e0fe4dc234549777efd3fbc9c6fd9ee0284719cd
WmmyS <wesleymarcosm@gmail.com>
2022-06-25 18:57:21 -0300

    feat: Adição de aulas.

---
+++ duvida.js
@@ -0,0 +1,7 @@
+const resultado1 = typeof NaN === typeof 1
+const resultado2 = typeof NaN === 'number'
+const resultado3 = typeof 1
+
+console.log('NAN is equlas not a number? :'+resultado1)
+console.log('Type of NAN is a number? :'+resultado2)
+console.log('Type of 1 is a number? :'+resultado2)
